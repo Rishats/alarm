d11c5ac36fec208e943833bb9cd921a425ca5314
Rishats <sultanov-rishat@inbox.ru>
2017-06-27 13:54:10 +0600

    Add key

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => env('APP_NAME', 'Alarm'),
+    'name' => env('APP_NAME', 'Alarm System'),
 
     /*
     |--------------------------------------------------------------------------
@@ -38,7 +38,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
