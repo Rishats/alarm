ff9e019496c7ba64bdc33b09f07e49616ed17bec
Rishats <sultanov-rishat@inbox.ru>
2018-05-19 15:01:02 +0600

    Fix last commit

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -42,7 +42,7 @@ class RegisterController extends Controller
     /**
      * Get a validator for an incoming registration request.
      *
-     * @param  array $data
+     * @param  array  $data
      * @return \Illuminate\Contracts\Validation\Validator
      */
     protected function validator(array $data)
@@ -56,11 +56,9 @@ class RegisterController extends Controller
     /**
      * Create a new user instance after a valid registration.
      *
-     * @param  array $data
+     * @param  array  $data
      * @return User
      */
-}
-
 
 /* Отключил регистрацию.
     protected function create(array $data)
@@ -71,5 +69,6 @@ class RegisterController extends Controller
             'password' => bcrypt($data['password']),
         ]);
     }
-}
 */
+}
+
