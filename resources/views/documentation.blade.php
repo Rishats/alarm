d538dfaabbc278043f3891e3148225d51228dcb0
Rishats <sultanov-rishat@inbox.ru>
2018-05-22 02:46:15 +0600

    FQA added

--- resources/views/documentation.blade.php
+++ resources/views/documentation.blade.php
@@ -6,5 +6,17 @@
 @endsection
 
 @section('contentappfunc')
-
+    <div>
+        <br>
+        <br>
+        <hr>
+        <h1>Разработчик</h1>
+        <p><a href="https://drive.google.com/open?id=19ujBCS549ls32-cMci6aUAOhdLfbnqKE" target="_blank">Презентация</a></p>
+        <p><a href="https://rishat-sultanov.ru" target="_blank">Ришат Султанов</a></p>
+        <p>Email: rishatdeveloper@gmail.com</p>
+        <iframe width="854" height="480" src="https://www.youtube.com/embed/CQk7eeMRJi0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        <br>
+        <br>
+        <hr>
+    </div>
 @endsection
